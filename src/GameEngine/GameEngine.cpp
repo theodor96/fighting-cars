ad9cbcafca1446b39b8a4745373d9b87f00d01e5
Daniel Maxim <Daniel Maxim>
2016-05-31 04:43:19 +0300

    fixed out of scene hearts

--- src/GameEngine/GameEngine.cpp
+++ src/GameEngine/GameEngine.cpp
@@ -117,28 +117,28 @@ void GameEngine::buildGui()
     mCountdownText->setPos(MAIN_WINDOW_WIDTH / 2 - 50, MAIN_WINDOW_HEIGHT / 2 - 150);
     mCountdownText->setScale(20);
 
-    QGraphicsSimpleTextItem* textNameBlue = mScene->addSimpleText(mPlayerMe->getUsername());
-    textNameBlue->setPos(4, 2);
+    QGraphicsSimpleTextItem* textNameBlue = mScene->addSimpleText(mPlayerMe->getUsername() + "a");
+    textNameBlue->setPos(4, 0);
     textNameBlue->setScale(2);
 
     for (quint8 i = 0; i < PLAYER_MAX_LIVES; i++)
     {
         vLivesBlue.push_back(mScene->addPixmap(QPixmap(":/img/img/bonus_1.png")));
-        vLivesBlue[i]->setPos(30 * i + 4, 30);
+        vLivesBlue[i]->setPos(30 * i + 4, 25);
         if (i >= PLAYER_DEFAULT_LIVES)
         {
             vLivesBlue[i]->setVisible(false);
         }
     }
 
-    QGraphicsSimpleTextItem* textNameRed = mScene->addSimpleText(mPlayerEnemy->getUsername());
-    textNameRed->setPos(4, MAIN_WINDOW_HEIGHT - 55);
+    QGraphicsSimpleTextItem* textNameRed = mScene->addSimpleText(mPlayerEnemy->getUsername() + "b");
+    textNameRed->setPos(4, MAIN_WINDOW_HEIGHT - 60);
     textNameRed->setScale(2);
 
     for (quint8 i = 0; i < PLAYER_MAX_LIVES; i++)
     {
         vLivesRed.push_back(mScene->addPixmap(QPixmap(":/img/img/bonus_1.png")));
-        vLivesRed[i]->setPos((30 * i) + 4, MAIN_WINDOW_HEIGHT - 25);
+        vLivesRed[i]->setPos((30 * i) + 4, MAIN_WINDOW_HEIGHT - 30);
         if (i >= PLAYER_DEFAULT_LIVES)
         {
             vLivesRed[i]->setVisible(false);
